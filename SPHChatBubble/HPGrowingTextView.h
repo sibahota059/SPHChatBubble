c7bb155df3669c186208a94f416b82daed8fd82d
Siba Hota <sibahota1989@gmail.com>
2014-03-12 20:50:06 +0530

    Update HPGrowingTextView.h

--- SPHChatBubble/HPGrowingTextView.h
+++ SPHChatBubble/HPGrowingTextView.h
@@ -27,6 +27,12 @@
 
 #import <UIKit/UIKit.h>
 
+#if __IPHONE_OS_VERSION_MAX_ALLOWED < 60000
+// UITextAlignment is deprecated in iOS 6.0+, use NSTextAlignment instead.
+// Reference: https://developer.apple.com/library/ios/documentation/uikit/reference/NSString_UIKit_Additions/Reference/Reference.html
+#define NSTextAlignment UITextAlignment
+#endif
+
 @class HPGrowingTextView;
 @class HPTextViewInternal;
 
@@ -50,7 +56,7 @@
 @end
 
 @interface HPGrowingTextView : UIView <UITextViewDelegate> {
-	HPTextViewInternal *internalTextView;	
+	HPTextViewInternal *internalTextView;
 	
 	int minHeight;
 	int maxHeight;
@@ -60,14 +66,16 @@
 	int minNumberOfLines;
 	
 	BOOL animateHeightChange;
+    NSTimeInterval animationDuration;
 	
 	//uitextview properties
 	NSObject <HPGrowingTextViewDelegate> *__unsafe_unretained delegate;
-	UITextAlignment textAlignment; 
+	NSTextAlignment textAlignment;
 	NSRange selectedRange;
 	BOOL editable;
 	UIDataDetectorTypes dataDetectorTypes;
 	UIReturnKeyType returnKeyType;
+	UIKeyboardType keyboardType;
     
     UIEdgeInsets contentInset;
 }
@@ -75,8 +83,13 @@
 //real class properties
 @property int maxNumberOfLines;
 @property int minNumberOfLines;
+@property (nonatomic) int maxHeight;
+@property (nonatomic) int minHeight;
 @property BOOL animateHeightChange;
-@property (nonatomic, strong) UITextView *internalTextView;	
+@property NSTimeInterval animationDuration;
+@property (nonatomic, strong) NSString *placeholder;
+@property (nonatomic, strong) UIColor *placeholderColor;
+@property (nonatomic, strong) UITextView *internalTextView;
 
 
 //uitextview properties
@@ -84,13 +97,19 @@
 @property(nonatomic,strong) NSString *text;
 @property(nonatomic,strong) UIFont *font;
 @property(nonatomic,strong) UIColor *textColor;
-@property(nonatomic) UITextAlignment textAlignment;    // default is UITextAlignmentLeft
+@property(nonatomic) NSTextAlignment textAlignment;    // default is NSTextAlignmentLeft
 @property(nonatomic) NSRange selectedRange;            // only ranges of length 0 are supported
 @property(nonatomic,getter=isEditable) BOOL editable;
 @property(nonatomic) UIDataDetectorTypes dataDetectorTypes __OSX_AVAILABLE_STARTING(__MAC_NA, __IPHONE_3_0);
 @property (nonatomic) UIReturnKeyType returnKeyType;
+@property (nonatomic) UIKeyboardType keyboardType;
 @property (assign) UIEdgeInsets contentInset;
+@property (nonatomic) BOOL isScrollable;
 @property(nonatomic) BOOL enablesReturnKeyAutomatically;
+
+#if __IPHONE_OS_VERSION_MIN_REQUIRED >= 70000
+- (id)initWithFrame:(CGRect)frame textContainer:(NSTextContainer *)textContainer;
+#endif
 
 //uitextview methods
 //need others? use .internalTextView
@@ -100,5 +119,8 @@
 
 - (BOOL)hasText;
 - (void)scrollRangeToVisible:(NSRange)range;
+
+// call to force a height change (e.g. after you change max/min lines)
+- (void)refreshHeight;
 
 @end

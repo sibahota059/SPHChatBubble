9724639032c8b421d752656a6ebc788f69b866f1
Siba Hota <sibahota1989@gmail.com>
2014-03-12 20:51:34 +0530

    Update HPTextViewInternal.m

--- SPHChatBubble/HPTextViewInternal.m
+++ SPHChatBubble/HPTextViewInternal.m
@@ -41,6 +41,11 @@
     [self setScrollEnabled:originalValue];
 }
 
+- (void)setScrollable:(BOOL)isScrollable
+{
+    [super setScrollEnabled:isScrollable];
+}
+
 -(void)setContentOffset:(CGPoint)s
 {
 	if(self.tracking || self.decelerating){
@@ -52,16 +57,20 @@
         self.contentInset = insets;
         
 	} else {
-
+        
 		float bottomOffset = (self.contentSize.height - self.frame.size.height + self.contentInset.bottom);
-		if(s.y < bottomOffset && self.scrollEnabled){            
+		if(s.y < bottomOffset && self.scrollEnabled){
             UIEdgeInsets insets = self.contentInset;
             insets.bottom = 8;
             insets.top = 0;
-            self.contentInset = insets;            
+            self.contentInset = insets;
         }
 	}
-    	
+    
+    // Fix "overscrolling" bug
+    if (s.y > self.contentSize.height - self.frame.size.height && !self.decelerating && !self.tracking && !self.dragging)
+        s = CGPointMake(s.x, self.contentSize.height - self.frame.size.height);
+    
 	[super setContentOffset:s];
 }
 
@@ -71,7 +80,7 @@
 	
 	if(s.bottom>8) insets.bottom = 0;
 	insets.top = 0;
-
+    
 	[super setContentInset:insets];
 }
 
@@ -89,7 +98,29 @@
     [super setContentSize:contentSize];
 }
 
+- (void)drawRect:(CGRect)rect
+{
+    [super drawRect:rect];
+    if (self.displayPlaceHolder && self.placeholder && self.placeholderColor)
+    {
+        if ([self respondsToSelector:@selector(snapshotViewAfterScreenUpdates:)])
+        {
+            NSMutableParagraphStyle *paragraphStyle = [[NSMutableParagraphStyle alloc] init];
+            paragraphStyle.alignment = self.textAlignment;
+            [self.placeholder drawInRect:CGRectMake(5, 8 + self.contentInset.top, self.frame.size.width-self.contentInset.left, self.frame.size.height- self.contentInset.top) withAttributes:@{NSFontAttributeName:self.font, NSForegroundColorAttributeName:self.placeholderColor, NSParagraphStyleAttributeName:paragraphStyle}];
+        }
+        else {
+            [self.placeholderColor set];
+            [self.placeholder drawInRect:CGRectMake(8.0f, 8.0f, self.frame.size.width - 16.0f, self.frame.size.height - 16.0f) withFont:self.font];
+        }
+    }
+}
 
-
+-(void)setPlaceholder:(NSString *)placeholder
+{
+	_placeholder = placeholder;
+	
+	[self setNeedsDisplay];
+}
 
 @end
